8c2896ef7398bd6d2480282406e551f0f21af619
BugHunter-X <askzenai@gmail.com>
2024-12-04 16:26:09 +0100

    Add about.js file

---
+++ about.js
@@ -0,0 +1,33 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+      <Link href='/about'>
+        <a>Go to About Page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+import { useRouter } from 'next/router';
+
+export default function About() {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push('/'); // Redirect to home page
+  };
+
+  return (
+    <div>
+      <h1>About Page</h1>
+      <button onClick={handleClick}>Go Home</button>
+    </div>
+  );
+}
+```
